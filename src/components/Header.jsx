df91a05c524625a24923c33d5ee8429e40037ce3
Stephanie Xue <stephaniexue3@gmail.com>
2026-07-15 01:15:47 -0700

    remove react header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,6 @@ function Header() {
                 alt="troll-face"
             />
             <h2 className="header-title">Meme Generator</h2>
-            <h4 className="header-project">React Course - Project 3</h4>
         </header>
     );
 }
